0afce62c35907d26e934f2835feba24c95536e7e
vihansr <vihansrathore2006@gmail.com>
2025-07-02 12:52:05 +0530

    selenium error

--- news.py
+++ news.py
@@ -6,18 +6,15 @@ from selenium.webdriver.chrome.options import Options
 
 class NewsScraper:
     def __init__(self):
-        self.driver = webdriver.Chrome()
-        self.overall_news = []
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--headless")  # Required for GitHub Actions
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_argument("--remote-debugging-port=9222")
-        chrome_options.add_argument("--disable-infobars")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--window-size=1920x1080")
 
-
+        self.driver = webdriver.Chrome(options=chrome_options)
+        self.overall_news = []
 
     def fetch_moneycontrol(self):
         try:
