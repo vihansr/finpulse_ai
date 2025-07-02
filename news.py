555e389c40360248f0ecb571e7458908f687870e
vihansr <vihansrathore2006@gmail.com>
2025-07-02 12:44:35 +0530

    update news

--- news.py
+++ news.py
@@ -2,11 +2,22 @@ from bs4 import BeautifulSoup as BS
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 class NewsScraper:
     def __init__(self):
         self.driver = webdriver.Chrome()
         self.overall_news = []
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--remote-debugging-port=9222")
+        chrome_options.add_argument("--disable-infobars")
+
+
 
     def fetch_moneycontrol(self):
         try:
